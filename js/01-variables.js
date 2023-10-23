e400842dd799e51c14ce487d2438c2000fb52cc1
Emilse Arias <emilsearias@gmail.com>
2023-10-23 18:35:06 -0300

    agregamos un ejemplo de variable creada con let

---
+++ js/01-variables.js
@@ -0,0 +1,14 @@
+// Esto es un comentario de una unica linea
+
+/* Esto es un comentario
+de multiples
+lineas */
+
+// declarar una variable (var - let - const)
+
+let anioActual = 2023;
+
+//mostrar mensajes
+console.log(anioActual);
+console.log("anioActual");
+console.log("Año actual: "+anioActual);
