86bc996747d217e889327f96ca4cb2e6a661b68b
Emilse Arias <emilsearias@gmail.com>
2023-10-25 17:20:19 -0300

    agregamos un mensaje con document.write y un alert

--- js/01-variables.js
+++ js/01-variables.js
@@ -7,8 +7,20 @@ lineas */
 // declarar una variable (var - let - const)
 
 let anioActual = 2023;
+const universe = "https://universe.rollingcodeschool.com/";
 
 //mostrar mensajes
 console.log(anioActual);
 console.log("anioActual");
-console.log("Año actual: "+anioActual);
+console.log("Año actual: " + anioActual);
+console.log(universe);
+
+//modificar el contenido de una variable
+anioActual = 2024;
+console.log(anioActual);
+
+document.write("<p class='parrafoDestacado'>Esto es una prueba de document.write</p>");
+
+//ventanas emergentes
+alert('Hola mundo');
+
