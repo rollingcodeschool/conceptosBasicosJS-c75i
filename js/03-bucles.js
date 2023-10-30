4f3df62f8d67bf7bdbc9c45fd062e0ce1e66063a
Emilse Arias <emilsearias@gmail.com>
2023-10-30 18:06:54 -0300

    agregamos un ejemplo de bucles while y do-while

---
+++ js/03-bucles.js
@@ -0,0 +1,31 @@
+/*
+while(condicion logica){
+    todo el codigo que quiero ejecutar varias veces
+    agregar algo para que la condicion logica no se cumpla
+}
+
+*/
+
+let renglon = 100;
+
+// document.write("<p>Este es el renglon número " + renglon + " </p>");
+
+while( renglon <= 20){
+    document.write(`<p>Este es el renglon número ${renglon} </p>`);
+    renglon++; // renglon = renglon + 1;
+}
+
+/*
+do{
+    todo el codigo que quiero ejecutar varias veces
+    agregar algo para que la condicion logica no se cumpla
+}while(condicion logica)
+*/
+
+let contador = 100;
+
+do{
+   
+    document.write(`<p>Este es el renglon número ${contador} (do-while)</p>`);
+    contador++; // renglon = renglon + 1;
+}while( contador <= 10)
