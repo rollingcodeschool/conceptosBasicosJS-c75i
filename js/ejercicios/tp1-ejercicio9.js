62c76749843109294b71b382317a5a9c1e1f0e7a
Emilse Arias <emilsearias@gmail.com>
2023-10-30 18:56:10 -0300

    agregamos la estructura del ejercicio 9 del tp 1

---
+++ js/ejercicios/tp1-ejercicio9.js
@@ -0,0 +1,7 @@
+// 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
+// Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
+
+const frase = prompt('Ingrese una frase');
+// hola mundo
+console.log(frase.length);
+console.log(frase.charAt(0));
