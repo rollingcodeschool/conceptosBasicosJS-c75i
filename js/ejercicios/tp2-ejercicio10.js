fba461df5f4c03b59b1f218c4e9da938a76c15b9
Emilse Arias <emilsearias@gmail.com>
2023-11-01 18:07:24 -0300

    agregamos el numero correspondiente a cada celda

--- js/ejercicios/tp2-ejercicio10.js
+++ js/ejercicios/tp2-ejercicio10.js
@@ -3,13 +3,14 @@
 const filas = parseInt(prompt('Ingrese una cantidad de filas'));
 const columnas = parseInt(prompt('Ingrese una cantidad de columnas'));
 
-const totalCeldas = filas * columnas;
+let totalCeldas = filas * columnas;
 
 document.write(`<table><tbody>`);
 for(let indiceFilas = 0; indiceFilas < filas; indiceFilas++){
     document.write(`<tr>`);
     for(let indiceColumnas = 0; indiceColumnas < columnas; indiceColumnas++){
-        document.write(`<td>1</td>`);
+        document.write(`<td>${totalCeldas}</td>`);
+        totalCeldas--; // totalCeldas = totalCeldas - 1;
     }
     document.write(`</tr>`);
 }
