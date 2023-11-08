d2dfdc3360a0a15a868a450970544685ea36e2a5
Emilse Arias <emilsearias@gmail.com>
2023-11-08 18:32:39 -0300

    agregamos ejemplos de funciones sin parametro, con parametro y que retornan un valor

---
+++ js/06-funciones.js
@@ -0,0 +1,29 @@
+//funciones declarativas
+function saludar(){
+    //todo el codigo que sabe hacer esa funcion
+    document.write('<p>Hola mundo</p>');
+}
+
+function saludoNuevo(nombreSolicitado, apellidoSolicitado){
+    document.write(`<p>Hola ${nombreSolicitado}, ${apellidoSolicitado} 😎</p>`);
+}
+
+function multiplicar(numero1, numero2){
+    const resultado = numero1 * numero2;
+    console.log(resultado);
+    return resultado;
+}
+
+//llamar o invocar a una funcion
+saludar();
+
+const nombre = prompt('Ingrese un nombre');
+const apellido = prompt('Ingrese un apellido');
+
+saludoNuevo(nombre, apellido);
+saludoNuevo('Jocker');
+saludoNuevo('Harry', 'Potter');
+
+const resultadoNuevo = multiplicar(30,3);
+document.write(`<p>El resultado de la multiplicación fue: ${resultadoNuevo}</p>`);
+document.write(`<p>El resultado de la multiplicación fue: ${multiplicar(45,3)}</p>`);
