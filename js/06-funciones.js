1162d0081102b0e693b1bf23cb246001cb476d4f
Emilse Arias <emilsearias@gmail.com>
2023-11-13 17:16:04 -0300

    agregamos un ejemplo de funcion en forma de flecha

--- js/06-funciones.js
+++ js/06-funciones.js
@@ -1,29 +1,44 @@
 //funciones declarativas
-function saludar(){
+function saludar() {
     //todo el codigo que sabe hacer esa funcion
-    document.write('<p>Hola mundo</p>');
-}
-
-function saludoNuevo(nombreSolicitado, apellidoSolicitado){
+    document.write("<p>Hola mundo</p>");
+  }
+  
+  function saludoNuevo(nombreSolicitado, apellidoSolicitado) {
     document.write(`<p>Hola ${nombreSolicitado}, ${apellidoSolicitado} 😎</p>`);
-}
+  }
 
-function multiplicar(numero1, numero2){
-    const resultado = numero1 * numero2;
-    console.log(resultado);
-    return resultado;
-}
+// function multiplicar(numero1, numero2){
+//     const resultado = numero1 * numero2;
+//     console.log(resultado);
+//     return resultado;
+// }
+
+//arrow functions
+// const multiplicar = (numero1, numero2) => {
+//     const resultado = numero1 * numero2;
+//     console.log(resultado);
+//     return resultado;
+//   };
+
+const multiplicar = (numero1, numero2) => numero1 * numero2;
 
 //llamar o invocar a una funcion
 saludar();
 
-const nombre = prompt('Ingrese un nombre');
-const apellido = prompt('Ingrese un apellido');
+const nombre = prompt("Ingrese un nombre");
+const apellido = prompt("Ingrese un apellido");
 
 saludoNuevo(nombre, apellido);
-saludoNuevo('Jocker');
-saludoNuevo('Harry', 'Potter');
+saludoNuevo("Jocker");
+saludoNuevo("Harry", "Potter");
 
-const resultadoNuevo = multiplicar(30,3);
-document.write(`<p>El resultado de la multiplicación fue: ${resultadoNuevo}</p>`);
-document.write(`<p>El resultado de la multiplicación fue: ${multiplicar(45,3)}</p>`);
+const resultadoNuevo = multiplicar(30, 3);
+document.write(
+  `<p>El resultado de la multiplicación fue: ${resultadoNuevo}</p>`
+);
+document.write(
+  `<p>El resultado de la multiplicación fue: ${multiplicar(45, 3)}</p>`
+);
+
+
