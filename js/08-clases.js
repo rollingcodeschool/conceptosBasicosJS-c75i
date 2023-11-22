c4960bc986fd667d80346e5a4db86810218ed4e3
Emilse Arias <emilsearias@gmail.com>
2023-11-22 17:24:42 -0300

    agregamos el metodo mostrarDatos en la clase pokemon

--- js/08-clases.js
+++ js/08-clases.js
@@ -12,6 +12,7 @@ class Pokemon{
         this.ataques = ataques;
         this.numPokedex = numPokedex;
         this.fuerza = fuerza;
+        this.estado = true; //valor por defecto
     }
     //propiedades computadas get y set
     get tipo(){
@@ -23,9 +24,20 @@ class Pokemon{
     }
 
     //metodos
+    mostrarDatos(){
+        document.write(`<h2>Pokemon: ${this.nombre}</h2>
+        <ul>
+        <li>Tipo: ${this.tipo}</li>
+        <li>Peso: ${this.peso}</li>
+        <li>Altura: ${this.altura}</li>
+        <li>Evolucion: ${this.evolucion}</li>
+        <li>Num Pokedex: ${this.numPokedex}</li>
+        </ul>`)
+    }
 };
 //crear o instanciar un objeto
 const charmander = new Pokemon('Charmander', 'fuego', 15, 1.20,['charmeleon', 'charizard'], ['blaze','ascuas','lanzallamas'], 4, 4);
+const togepi = new Pokemon ('Togepi', 'hada', 1.5, 0.3, ['Togetic','togekiss'], ['gracia serena', 'dicha y entusiasmo', 'placaje'], 175, 2)
 
 console.log(charmander)
 document.write(`<h2>Pokemon: ${charmander.nombre}</h2>`);
@@ -34,4 +46,6 @@ document.write(`<p>Tipo: ${charmander.tipo}</p>`);
 charmander.tipo = 'fuego 2'
 document.write(`<p>Tipo: ${charmander.tipo}</p>`);
 
+charmander.mostrarDatos();
 
+togepi.mostrarDatos();
